a8d3adb485fd3929c8e7ad51994ba8cc54fd1f10
Rodrigo Bossini <professorbossini@gmail.com>
2020-05-30 13:11:05 -0300

    terminamos o pdf de data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maua-teste-data-binding';
+
+  nome;
+  esconderCaixa = true;
+  produto;
+
+  alterarNome(evento) {
+    console.log(evento.target.value);
+    this.nome = evento.target.value;
+  }
+  adicionar(nome) {
+    console.log(nome);
+    this.nome = nome;
+    console.log("Adicionando...");
+    this.esconderCaixa = false;
+  }
+
+  alterarProduto(produtoInput) {
+    this.produto = "Novo Produto: " + produtoInput.value;
+  }
+  /*nome = "Ana";
+  idade = 20;
+
+  lancarDado() {
+    return Math.floor(Math.random() * 6) + 1;
+  }*/
 }
